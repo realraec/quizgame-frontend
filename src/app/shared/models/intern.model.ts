2c13a0387a70b297e92c4ecddf2b3e00d877afa5
juniordav <juniordavon1998@gmail.com>
2023-02-14 17:03:20 +0100

    feat(quizgame-frontend): create models and services

---
+++ src/app/shared/models/intern.model.ts
@@ -0,0 +1,10 @@
+export interface Intern {
+  id?: number;
+  username?: string;
+  password?: string;
+  firstname?: string;
+  laststname?: string;
+  email?: string;
+  role?: string;
+  company?: string;
+}
