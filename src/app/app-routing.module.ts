e7298944fc600d79b8c82b115443778e835e9595
juniordav <juniordavon1998@gmail.com>
2023-02-14 08:21:27 +0100

    feat(quizgame-fronentend): create login componenent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {path: '', component: LoginComponent},
   { path: 'administrator', data: { breadcrumb: 'Espace Administrateur' }, loadChildren: () => import('./administrator/administrator.module').then(m => m.AdministratorModule) },
   { path: 'intern', data: { breadcrumb: 'Espace Stagiaire' }, loadChildren: () => import('./intern/intern.module').then(m => m.InternModule) },
 
